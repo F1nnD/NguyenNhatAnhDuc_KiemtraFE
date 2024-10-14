bd2531363d9c4cfcb9ffec577182f0efa5cf3c8d
nguye <nguye@DESKTOP-U7LAM4H>
2024-10-14 21:24:54 +0700

    Add project files.

---
+++ NguyenNhatAnhDuc_KiemtraFE/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace NguyenNhatAnhDuc_KiemtraFE.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Trang chủ, auto bump khi mở Solution 
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+} 
